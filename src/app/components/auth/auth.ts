fee2e9733a8850129f327387cbbd1ee64477ccc5
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-04-24 23:05:16 +0500

    fix console import

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -2,7 +2,6 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { error } from 'console';
 
 
 @Component({
