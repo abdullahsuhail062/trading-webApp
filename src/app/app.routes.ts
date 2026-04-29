935287c0eb0ef1371e1a01651979dfba428feb89
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-04-29 22:05:54 +0500

    Update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing/landing';
 import { AuthComponent } from './components/auth/auth';
-
+import { Dashboard } from './components/auth/dashboard/dashboard';
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'auth', component: AuthComponent },
+  {path: 'dashboard', component: Dashboard },
   { path: '**', redirectTo: '' }
 ];
