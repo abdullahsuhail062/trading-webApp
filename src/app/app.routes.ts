8cc91f35d1841b16f3edfe0b929d94d568c92b5b
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-02 22:55:09 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import {publicGuard} from './auth/public-guard'
-import {authGuard} from './auth/auth-guard'
+import {PublicGuard} from './auth/public-guard'
+import {AuthGuard} from './auth/auth-guard'
 import { LandingComponent } from './components/landing/landing';
 import { AuthComponent } from './components/auth/auth';
 import { Dashboard } from './components/dashboard/dashboard';
 import { from } from 'rxjs';
 export const routes: Routes = [
-  { path: '', component: LandingComponent, canActivate: [publicGuard] },
-  {path: 'auth', component: AuthComponent, canActivate: [publicGuard]},
-    {path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: '', component: LandingComponent, canActivate: [PublicGuard] },
+  {path: 'auth', component: AuthComponent, canActivate: [PublicGuard]},
+    {path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
