619f41e63643602578b9d49fd7439ed86be28645
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-02 22:50:37 +0500

    update

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,10 +1,26 @@
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+// import { CanActivateFn, Router } from '@angular/router';
+// import { AuthService } from '../services/auth.service';
+// import { inject } from '@angular/core';
+
+
+// export const authGuard: CanActivateFn = (route, state) => {
+//   const isLoggedIn = inject(AuthService).isLoggedIn();
+//   const router = inject(Router)
+//   return true; 
+// };
+
+
 import { inject } from '@angular/core';
+import { Router, CanMatchFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-
-export const authGuard: CanActivateFn = (route, state) => {
+// ✅ protects dashboard — no flash
+export const AuthGuard: CanMatchFn = () => {
   const isLoggedIn = inject(AuthService).isLoggedIn();
-  const router = inject(Router)
-  return true; 
+  const router = inject(Router);
+
+  if (isLoggedIn) {
+    return true;
+  }
+  return router.createUrlTree(['/auth']);
 };
--- src/app/auth/public-guard.ts
+++ src/app/auth/public-guard.ts
@@ -1,11 +1,22 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn, Router, CanMatchFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
 
-export const publicGuard: CanActivateFn = (route, state) => {
+// export const publicGuard: CanActivateFn = (route, state) => {
+//   const isLoggedIn = inject(AuthService).isLoggedIn();
+//   const router = inject(Router)
+//   return isLoggedIn ? router.parseUrl('/dashboard'):
+//   true;
+// };
+
+
+export const PublicGuard: CanMatchFn = () => {
   const isLoggedIn = inject(AuthService).isLoggedIn();
-  const router = inject(Router)
-  return isLoggedIn ? router.parseUrl('/dashboard'):
-  true;
+  const router = inject(Router);
+
+  if (isLoggedIn) {
+    return router.createUrlTree(['/dashboard']);
+  }
+  return true;
 };
