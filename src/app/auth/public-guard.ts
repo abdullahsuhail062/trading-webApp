c36fa468f47003be9cc05c29f2479bfe2f5438ce
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-14 13:30:07 +0500

    update

--- src/app/auth/public-guard.ts
+++ src/app/auth/public-guard.ts
@@ -5,12 +5,7 @@ import { filter, map, take } from 'rxjs';
 
 
 
-// export const publicGuard: CanActivateFn = (route, state) => {
-//   const isLoggedIn = inject(AuthService).isLoggedIn();
-//   const router = inject(Router)
-//   return isLoggedIn ? router.parseUrl('/dashboard'):
-//   true;
-// };
+
 
 
 export const PublicGuard: CanMatchFn = () => {
