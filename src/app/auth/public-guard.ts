94a00bd464135772d1ba947ccbbca6feea5045c0
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-15 10:30:24 +0500

    update

--- src/app/auth/public-guard.ts
+++ src/app/auth/public-guard.ts
@@ -1,21 +1,21 @@
-import { CanActivateFn, Router, CanMatchFn } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { inject } from '@angular/core';
-import { filter, map, take } from 'rxjs';
+// import { CanActivateFn, Router, CanMatchFn } from '@angular/router';
+// import { AuthService } from '../services/auth.service';
+// import { inject } from '@angular/core';
+// import { filter, map, take } from 'rxjs';
 
 
 
 
 
 
-export const PublicGuard: CanMatchFn = () => {
-  const auth = inject(AuthService)
-  const router = inject(Router);
+// export const PublicGuard: CanActivateFn = () => {
+//   const auth = inject(AuthService)
+//   const router = inject(Router);
 
   
-    return auth.isInitialized$.pipe(
-      filter(ready => ready), // Wait for initialization to be true
-      take(1),                // Grab the value and complete the stream
-      map(() => auth.isLoggedIn() ? true : router.parseUrl('/login'))
-    );
-};
+//     return auth.isInitialized$.pipe(
+//       filter(ready => ready), // Wait for initialization to be true
+//       take(1),                // Grab the value and complete the stream
+//       map(() => auth.isLoggedIn() ? true : router.parseUrl('/login'))
+//     );
+// };
