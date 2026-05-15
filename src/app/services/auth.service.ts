dd88348b1a0aced4f27d61569489be1bba2bf4a3
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-16 00:25:04 +0500

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   readonly isLoggedIn = computed(() => this.authStore.isLoggedIn());
 
   constructor(private apiService: ApiService) {
-    //this.initUser();
+    this.initUser();
   }
 
   
