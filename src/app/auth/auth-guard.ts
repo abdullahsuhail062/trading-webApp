d999bf29576183f65370d63890553e667bfe6cbc
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-15 10:45:05 +0500

    update

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,17 +1,17 @@
-// import { inject } from '@angular/core';
-// import { Router, CanMatchFn, CanActivateFn } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-// import { filter, map, take } from 'rxjs';
+import { inject } from '@angular/core';
+import { Router, CanMatchFn, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { filter, map, take } from 'rxjs';
 
 
-// // ✅ protects dashboard — no flash
-// export const AuthGuard: CanActivateFn = () => {
-//   const auth = inject(AuthService);
-//   const router = inject(Router);
+// ✅ protects dashboard — no flash
+export const AuthGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
 
-//   return auth.isInitialized$.pipe(
-//     filter(ready => ready), // Wait for initialization to be true
-//     take(1),                // Grab the value and complete the stream
-//     map(() => auth.isLoggedIn() ? true : router.parseUrl('/login'))
-//   );
-// };
+  return auth.isInitialized$.pipe(
+    filter(ready => ready), // Wait for initialization to be true
+    take(1),                // Grab the value and complete the stream
+    map(() => auth.isLoggedIn() ? true : router.parseUrl('/login'))
+  );
+};
